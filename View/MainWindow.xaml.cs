fcbc2e57548bcabdd2fd9f9c05bffde165b14fe4
zedicus25 <Ciga_fu80@student.itstep.org>
2022-10-12 19:05:00 +0300

    Add material design and another extensions

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace EmployeeManagement
+namespace EmployeeManagement.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml

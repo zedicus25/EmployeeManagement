bd9beaf974a307290a0a7920a27ac169228bba52
Alexander Tsyganok <Ciga_fu80@student.itstep.org>
2022-10-18 17:42:35 +0300

    Фв

--- Server/Controller/UserController.cs
+++ Server/Controller/UserController.cs
@@ -17,12 +17,12 @@ namespace Server.Controller
             Users = _jsonDataController.ReadData();
         }
 
-        public string CheckData(string data)
+        public bool CheckData(string data)
         {
             data = data.Trim();
             string[] datas = data.Split('\n');
             if (datas.Length != 3)
-                return "false";
+                return false;
             for (int i = 0; i < datas.Length; i++)
             {
                 datas[i] = datas[i].Trim();
@@ -34,9 +34,9 @@ namespace Server.Controller
                 string password = datas[2].Substring(datas[2].IndexOf('=')+1).Trim();
                 User user = Users.FirstOrDefault(u => u.Password.Equals(password) && u.Login.Equals(login));
                 if (user != null)
-                    return "true";
+                    return true;
             }
-            return "false";
+            return false;
         }
     }
 }

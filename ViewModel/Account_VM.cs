d01c763e114d9ffab47cc2cdcc54a85ab4b5c8b9
Alexander Tsyganok <Ciga_fu80@student.itstep.org>
2022-10-21 12:19:31 +0300

    Add account page design and data binding

--- ViewModel/Account_VM.cs
+++ ViewModel/Account_VM.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EmployeeManagement.Model;
+using System;
+
 
 namespace EmployeeManagement.ViewModel
 {
     public class Account_VM : BaseVM
     {
+        public User User 
+        {
+            get => _user;
+            set 
+            { 
+                _user = value;
+                OnPropertyChanged("User");
+            }
+        }
+
+        private User _user;
+
+        public Account_VM()
+        {
+            _user = MainViewModel.Instance.User;
+        }
     }
 }

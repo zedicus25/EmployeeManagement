0ebb5d52043dbd146a13f7f5a9e190af53071da7
Olexander Tsyganok <Ciga_fu80@student.itstep.org>
2022-12-05 10:29:13 +0200

    Start adding home page graphic

--- Client(Admin)/ViewModel/HomePageVM.cs
+++ Client(Admin)/ViewModel/HomePageVM.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Client_Admin_.ViewModel
 {
     public class HomePageVM : BaseVM
     {
+
+        private CancellationTokenSource _tokenSource;
+        private Task _vmCreation;
+        private List<BaseVM> _allVMs;
+        public HomePageVM()
+        {
+            _tokenSource = new CancellationTokenSource();
+            _allVMs = new List<BaseVM>();
+            _vmCreation = new Task(CreateVMs, _tokenSource.Token);
+        }
+
+        private async void CreateVMs()
+        {
+            while (MainVM.GetInstance().User == null)
+                await Task.Delay(10);
+
+            _tokenSource.Cancel();
+        }
     }
 }

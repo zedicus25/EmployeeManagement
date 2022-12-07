a59f3b40ef90b2c0a9b9ed3ab8647950aef6a403
Olexander Tsyganok <Ciga_fu80@student.itstep.org>
2022-12-07 19:09:25 +0200

    add background getting my tasks

--- Client(User)/ViewModel/MainViewModel.cs
+++ Client(User)/ViewModel/MainViewModel.cs
@@ -56,7 +56,10 @@ namespace EmployeeManagement.ViewModel
 			_querryDelay = new TimeSpan(0, 5, 0);
             SelectedViewModel = new LoginWindow_VM();
             _tokenSourceListenTasks = new CancellationTokenSource();
-            
+			_tokenSourceListenMyTasks = new CancellationTokenSource();
+            _listenAllTask = new Task(SendAllTasksQuerry, _tokenSourceListenTasks.Token);
+            _listenMyTask = new Task(SendMyTasksQuerry, _tokenSourceListenMyTasks.Token);
+
         }
 
 		public void ConnectToServer()
@@ -65,8 +68,10 @@ namespace EmployeeManagement.ViewModel
 				return;
 
             ServerClient = new ServerClient();
-            _listenAllTask = new Task(SendAllTasksQuerry, _tokenSourceListenTasks.Token);
+
+            
             _listenAllTask.Start();
+            _listenMyTask.Start();
         }
 
 		
@@ -92,6 +97,14 @@ namespace EmployeeManagement.ViewModel
             while (_tokenSourceListenTasks.Token.IsCancellationRequested == false)
             {
                 ServerClient.GetAllTasks();
+                await Task.Delay(_querryDelay);
+            }
+        }
+        private async void SendMyTasksQuerry()
+        {
+            while (_tokenSourceListenTasks.Token.IsCancellationRequested == false)
+            {
+                ServerClient.GetMyTask();
                 await Task.Delay(_querryDelay);
             }
         }
